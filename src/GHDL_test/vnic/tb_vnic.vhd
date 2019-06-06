a06256cdd6d6f2b73dbb8141e4e39fa639b488b5
Gentile, John <johncgentile17@gmail.com>
2019-06-06 16:09:05 -0400

    (+) hex values printout
    Signed-off-by: Gentile, John <johncgentile17@gmail.com>

--- src/GHDL_test/vnic/tb_vnic.vhd
+++ src/GHDL_test/vnic/tb_vnic.vhd
@@ -1,5 +1,9 @@
 library ieee;
   use ieee.std_logic_1164.all;
+  use ieee.std_logic_unsigned.all;
+  use ieee.numeric_std.all;
+
+  use std.textio.all;
 
   use work.pkg_net.all;
 
@@ -15,8 +19,10 @@ begin
   begin
     rx_length := F_receive_pkt;
     wait for 1 ns;
-    for i in 0 to rx_length - 1 loop
+    -- each iteration is 32 bits (4 bytes) since integer data types used
+    for i in 0 to (rx_length/4) - 1 loop
       p_tx_buff(i) := p_rx_buff(i);
+      report "RX Pkt Data [" & integer'image(i) & "]: 0x" & to_hstring(to_signed(p_rx_buff(i), 32)) severity note;
     end loop;
     wait for 1 ns;
     if F_send_pkt(rx_length) > 0 then
