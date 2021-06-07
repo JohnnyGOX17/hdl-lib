10314225284baa845027d2477d75fddc39a5f95e
Gentile, John <johncgentile17@gmail.com>
2021-06-06 21:33:08 -0500

    iverilog -> GTKWave updates
    Signed-off-by: Gentile, John <johncgentile17@gmail.com>

--- IO_interfaces/AXI/AXI-Stream/tb_AXIS_reg_slice.v
+++ IO_interfaces/AXI/AXI-Stream/tb_AXIS_reg_slice.v
@@ -16,20 +16,53 @@ module tb_AXIS_reg_slice;
   // reset and test data generation
   integer i;
   initial begin
+    // dump value changes of nets & regs to *.vcd file for waveform viewing
+    $dumpfile("test.vcd");
+    // <levels>, <module_or_variable> http://www.referencedesigner.com/tutorials/verilog/verilog_62.php
+    $dumpvars(0,tb_AXIS_reg_slice);
+    // monitor tready to upstream logic from DUT
+    $monitor("s_axis_tready = %b, @ %0t", s_axis_tready, $time);
     // initial values
     reset         = 0;
     s_axis_tvalid = 0;
     s_axis_tdata  = 32'h0000_0000;
-    m_axis_tready = 1; // always ready for now...
+    m_axis_tready = 0; // always ready for now...
 
     #10 reset = 1;
     #10 reset = 0;
 
+    // Test Case 1: downstream/slave always ready for data
+    m_axis_tready = 1; // always ready for now...
+    $display("Test Case 1: downstream/slave always ready for data");
     for (i=0; i < 16; i = i + 1) begin
+      // wait for reg slice ready
+      while ( s_axis_tready == 0 )
+        #10 forever;
+
       $display("\tAXI-S Data Input: 0x%0H, @ %0t", i, $time);
-      #10 s_axis_tvalid = 1;
-      #10 s_axis_tdata  = i;
-      #5  s_axis_tvalid = 0;
+      s_axis_tvalid = 1;
+      s_axis_tdata  = i;
+      #10 s_axis_tvalid = 0;
+      #10 s_axis_tdata  = 0;
+    end
+
+    // inter-test wait...
+    #10 m_axis_tready = 0; // start w/downstream NOT ready for this test...
+    #10 s_axis_tvalid = 0;
+
+    // Test Case 2: downstream/slave ready after 2x cycles of valid data
+    $display("Test Case 2: downstream/slave ready after 2x cycles of valid data");
+    for (i=0; i < 16; i = i + 1) begin
+      if (i > 1)
+        m_axis_tready = 1;
+      // wait for reg slice ready
+      while ( s_axis_tready == 0 )
+        #10 forever;
+
+      $display("\tAXI-S Data Input: 0x%0H, @ %0t", i, $time);
+      s_axis_tvalid = 1;
+      s_axis_tdata  = i;
+      #10 s_axis_tvalid = 0;
       #10 s_axis_tdata  = 0;
     end
 
@@ -43,15 +76,12 @@ module tb_AXIS_reg_slice;
   end
 
 
-  // monitor tready from DUT
-  initial
-    $monitor("s_axis_tready = %b, @ %0t", s_axis_tready, $time);
 
-  // synchronous monitor of data output
+  // synchronous monitor of data output when valid consumption of data
   always @(posedge clk) begin
     // %0t removes unnecessary padding
-    if (m_axis_tvalid)
-      $display("AXI-S Data Output: 0x%0H, @ %0t", m_axis_tdata, $time);
+    if (m_axis_tvalid && m_axis_tready)
+      $display("\t\tAXI-S Data Output: 0x%0H, @ %0t", m_axis_tdata, $time);
   end
 
 
